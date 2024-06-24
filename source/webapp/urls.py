438d6f640e57b4fc8893fea43041d5397c8e1fbb
”Kirill <kirillgrekhovodov@gmail.com>
2024-06-24 20:01:23 +0600

    задал имена роутам, использовал их во views и шаблонах
    также обработал ситуацию когда статья не найдена, возращаю 404 ошибку

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from webapp.views import index, create_article, article_detail
 
 urlpatterns = [
-    path('', index),
-    path('create/', create_article),
-    path('article/<int:pk>/', article_detail),
+    path('', index, name='articles'),
+    path('create/', create_article, name='create_article'),
+    path('article/<int:pk>/', article_detail, name='article_detail'),
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponseNotFound, Http404
+from django.urls import reverse
 
 from webapp.models import Article
 
@@ -13,18 +14,22 @@ def create_article(request):
     if request.method == "GET":
         return render(request, "create_article.html")
     else:
-        Article.objects.create(
+        article = Article.objects.create(
             title=request.POST.get("title"),
             content=request.POST.get("content"),
             author=request.POST.get("author")
         )
-        return HttpResponseRedirect("/")
+        # return HttpResponseRedirect(reverse("articles"))
+        # return HttpResponseRedirect(
+        #     reverse("article_detail", kwargs={"pk": article.pk})
+        # )
+        return redirect("article_detail", pk=article.pk)
 
 
 def article_detail(request, *args, pk, **kwargs):
-    print(kwargs)
-    try:
-        article = Article.objects.get(id=pk)
-    except Article.DoesNotExist:
-        return HttpResponseRedirect("/")
+    article = get_object_or_404(Article, pk=pk)
+    # try:
+    #     article = Article.objects.get(id=pk)
+    # except Article.DoesNotExist:
+    #     raise Http404
     return render(request, "article_detail.html", context={"article": article})
