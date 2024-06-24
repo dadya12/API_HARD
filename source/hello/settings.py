f488dc00c6e67d4e5783311d433131464be792bd
”Kirill <kirillgrekhovodov@gmail.com>
2024-06-24 19:24:14 +0600

    поменял базу данных на postgres

--- source/hello/settings.py
+++ source/hello/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'hello.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'article_project',
+        'USER': 'kirill',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
