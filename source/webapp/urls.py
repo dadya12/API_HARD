bfdb05efbe87d4ff121f5b107db2b031096a50a2
”Kirill <kirillgrekhovodov@gmail.com>
2024-07-01 19:44:11 +0600

    добавил редактирование статьи и удаление с подтверждением

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from webapp.views import index, create_article, article_detail
+from webapp.views import index, create_article, article_detail, update_article, delete_article
 
 urlpatterns = [
     path('', index, name='articles'),
     path('create/', create_article, name='create_article'),
     path('article/<int:pk>/', article_detail, name='article_detail'),
+    path('article/<int:pk>/update/', update_article, name='update_article'),
+    path('article/<int:pk>/delete/', delete_article, name='delete_article'),
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -23,3 +23,27 @@ def create_article(request):
 def article_detail(request, *args, pk, **kwargs):
     article = get_object_or_404(Article, pk=pk)
     return render(request, "article_detail.html", context={"article": article})
+
+
+def update_article(request, *args, pk, **kwargs):
+    if request.method == "GET":
+        return render(
+            request, "update_article.html",
+            context={"article": get_object_or_404(Article, pk=pk)}
+        )
+    else:
+        article = get_object_or_404(Article, pk=pk)
+        article.title = request.POST.get("title")
+        article.content = request.POST.get("content")
+        article.author = request.POST.get("author")
+        article.save()
+        return redirect("article_detail", pk=article.pk)
+
+
+def delete_article(request, *args, pk, **kwargs):
+    article = get_object_or_404(Article, pk=pk)
+    if request.method == "GET":
+        return render(request, "delete_article.html", context={"article": article})
+    else:
+        article.delete()
+        return redirect("articles")
